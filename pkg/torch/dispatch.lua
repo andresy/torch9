ac6bb9d15482581195698a7bc466213a0621af7a
Ronan Collobert <ronan@collobert.com>
2012-12-13 18:02:32 +0100

    dispatch.lua: handles better optional dispatch function

--- pkg/torch/dispatch.lua
+++ pkg/torch/dispatch.lua
@@ -3,26 +3,27 @@ dispatch =
    argcheck{
    {{name="idx", type="number", default=1}},
    function(idx)
-      local env = {idx=idx, funcs={}, type=type, select=select, error=error}
-      local func
-      if idx > 0 then
-         func =
-            function(...)
-               local typename = type(select(idx, ...))
-               local func = funcs[typename]
-               if not func then
-                  error('function not implemented for type ' .. typename)
-               else
-                  return func(...)
-               end
+      local env = {idx=idx, funcs={}, type=type, select=select, error=error, string=string}
+      local func =
+         function(...)
+            local typename = type(select(idx, ...))
+            local func = funcs[typename]
+            if not func then
+               error(string.format('function not implemented for type <%s>', typename))
+            else
+               return func(...)
             end
-      else
-         func = 
-            function(...)
-            end
-      end
+         end
       setfenv(func, env)
       return func
+   end,
+
+   {{name="idxfunc", type="function"}},
+   function(idxfunc)
+      local env = {funcs={}}
+      setmetatable(env, {__index=getfenv(1)})
+      setfenv(idxfunc, env)
+      return idxfunc
    end,
    
    {{name="func", type="function"},
