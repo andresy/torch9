13e007ffd95a79245db608827167d1214d36129e
Ronan Collobert <ronan@collobert.com>
2012-12-29 14:54:55 +0100

    cleaned up type

--- pkg/torch/init.lua
+++ pkg/torch/init.lua
@@ -11,35 +11,7 @@ typedef unsigned char byte;
 torch = {}
 package.loaded.torch = torch
 
-function torch.type(obj)
-   local tname = type(obj)
-   if tname == 'table' then
-      return obj.__typename or tname
-   end
-   return tname
-end
-torch.typename = torch.type -- backward compatibility... keep it or not?
-
-function torch.istype(obj, typename)
-   local tname = type(obj)
-   if tname == 'table' then
-      if obj.__typename then
-         obj = getmetatable(obj)
-         while type(obj) == 'table' do
-            if obj.__typename == typename then
-               return true
-            else
-               obj = getmetatable(obj)
-            end
-         end
-         return false
-      else
-         return tname == typename
-      end
-   else
-      return typename == tname
-   end
-end
+require 'torch.type'
 
 function torch.class(tname, parenttname)
 
